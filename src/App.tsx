65907a66185693378d37c12990034f87f818956b
Abduahad <abduahadquyliyev001@gmail.com>
2024-03-08 18:08:34 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+// import axios from 'axios'
+// import apiClient from './service/api.client'
+import { Button } from "@radix-ui/themes";
+
+const App = () => {
+  // apiClient.post('/singup', {
+  //   headers:{},
+  //   body: JSON.stringify({}),
+  // })
+  // // console.log(axios);
+  //  axios
+  //  .get('https://jsonplaceholder.typcode.com/users')
+  //  .then(res => console.log(res.data))
+  //  .catch(err => console.error(err.message))
+
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default App;
